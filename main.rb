c496a35050a23a3cf369da47db7301f09117a637
Daman Bual <damanbual@gmail.com>
2024-11-01 18:02:43 -0700

    Complete candidate filtering and ordering

--- main.rb
+++ main.rb
@@ -7,3 +7,11 @@ require './filters'
 ## Your test code can go here
 
 pp @candidates
+
+puts experienced?(@candidates[0]) # Should return true or false based on the experience level of the first candidate
+
+pp find(5) # Should return the candidate with id 5
+
+pp qualified_candidates(@candidates) # Should print only the candidates who meet all criteria
+
+pp ordered_by_qualifications(@candidates)
